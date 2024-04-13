7032eeb2e1edfa8431751751838423085f820741
Ashleigh Day <day.ashleigh@gmail.com>
2024-04-13 23:04:40 +0100

    Committed latest version of the code.

--- ElgatoControlCentre_Backend/main.py
+++ ElgatoControlCentre_Backend/main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+import requests
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = "http://192.168.0.10:9123/elgato/lights"
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# Application Entry Point
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Enter the number of lights")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    numberOfLights = int(input("Number of Lights: "))
+
+    print("Power status of the light, enter 1 to turn on and 0 to turn off")
+
+    lightPower = int(input("Power status of the device: "))
+
+    print("Enter the light brightness value")
+
+    lightBrightness = int(input("Brightness Value: "))
+
+    print("Enter the light temperature value")
+
+    lightTemperature = int(input("Temperature Value: "))
+
+    lights = []
+
+    lights.append({
+        "on": lightPower,
+        "brightness": lightBrightness,
+        "temperature": lightTemperature
+    })
+
+    '''
+    payload = json.dumps({
+        "numberOfLights": 1,
+        "lights": [
+            {
+                "on": 0,
+                "brightness": 32,
+                "temperature": 227
+            }
+        ]
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request("PUT", url, headers=headers, data=payload)
+    
+    '''
+
+    payload = json.dumps({
+        "numberOfLights": numberOfLights,
+        "lights": lights
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("PUT", url, headers=headers, data=payload)
+
+    print(response.text)
